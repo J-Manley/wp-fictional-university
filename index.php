1b01180a501c7d860d7e1c52e341ae8acce760dd
Joel Manley <j_90@live.co.uk>
2022-02-15 17:14:26 +0000

    add posts & page

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <?php
-function greet($name, $color)
-{
-    echo "<p>Hi, my name is $name and my favourite colour is $color</p>";
+
+while (have_posts()) {
+    the_post(); ?>
+    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <?php the_content(); ?>
+    <hr>
+<?php
 }
 
-greet("John", "blue");
-greet("Jane", "green");
 ?>
-
-
- ?>
