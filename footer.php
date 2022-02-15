a054c69f3c3cc4758b6778cb2175eccff29fe89e
Joel Manley <j_90@live.co.uk>
2022-02-15 21:21:43 +0000

    add header, footer & functions

---
+++ footer.php
@@ -0,0 +1,5 @@
+<h1>Footer text</h1>
+
+<?php wp_footer(); ?>
+</body>
+</html>
--- page.php
+++ page.php
@@ -1,4 +1,4 @@
-<?php
+<?php get_header();
 
 while (have_posts()) {
     the_post(); ?>
@@ -6,4 +6,5 @@ while (have_posts()) {
     <h2><?php the_title(); ?></h2>
     <?php the_content(); ?>
 <?php
-} ?>
+}
+get_footer(); ?>
